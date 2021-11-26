f1a975ecf8612d5ba584594cbf4fa97a9129dc42
Gabriele Almeida <naogabe@gmail.com>
2021-11-26 16:58:24 -0300

    refactor: Atualizando qtde tweets e seguidores de forma dinamica

--- home.php
+++ home.php
@@ -4,6 +4,42 @@
     if(!isset($_SESSION['usuario'])){
         header('Location: index.php?erro=2');
     }
+
+	require_once('db.class.php');
+
+	$objDB = new DB();
+    $link = $objDB->conecta_mysql();
+	
+	$id_usuario = $_SESSION['id_usuario'];
+
+	/* Recuperar qtd tweets */
+	$sql = "SELECT COUNT(*) AS qtde_tweets FROM tweets WHERE id_usuario = $id_usuario";
+	$resultado = mysqli_query($link, $sql);
+	$qtde_tweets = 0;
+
+	if($resultado){
+        
+        $registro = mysqli_fetch_array($resultado, MYSQLI_ASSOC);		 
+		$qtde_tweets = $registro['qtde_tweets'];
+
+    } else {
+        echo "Erro na consulta de registros no banco de dados!";
+    }
+	/* Recuperar qtd seguidores */
+	$sql = "SELECT COUNT(*) AS qtde_seguidores FROM usuarios_seguidores
+			WHERE seguindo_id_usuario = $id_usuario";
+	$resultado = mysqli_query($link, $sql);
+	$qtde_seguidores = 0;
+
+	if($resultado){
+        
+        $registro = mysqli_fetch_array($resultado, MYSQLI_ASSOC);		 
+		$qtde_seguidores = $registro['qtde_seguidores'];
+
+    } else {
+        echo "Erro na consulta de registros no banco de dados!";
+    }
+
 ?>
 
 <!DOCTYPE HTML>
@@ -87,10 +123,10 @@
 					<h4><?=  $_SESSION['usuario']; ?></h4>
 					<hr>
 					<div class="col-md-6">
-						TWEETS <br> 1
+						TWEETS <br> <?= $qtde_tweets ?>
 					</div>
 					<div class="col-md-6">
-						SEGUIDORES <br> 1
+						SEGUIDORES <br> <?= $qtde_seguidores ?>
 					</div>
 				</div>
 			</div>
